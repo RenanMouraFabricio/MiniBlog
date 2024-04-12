3d5b949c745519e58bb150df79bff6a044c530a4
RenanMouraFabricio <165737048+RenanMouraFabricio@users.noreply.github.com>
2024-04-12 17:56:26 -0300

    Update __init__.py

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -1,9 +1,9 @@
 import sqlalchemy
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import os
 
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
