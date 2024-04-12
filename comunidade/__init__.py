7f1727035f4e547f742b0bd648f1a80596525d41
RenanMouraFabricio <165737048+RenanMouraFabricio@users.noreply.github.com>
2024-04-12 17:52:19 -0300

    Update __init__.py

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -1,8 +1,10 @@
+import sqlalchemy
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+
 
 app = Flask(__name__)
 
@@ -18,5 +20,17 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from comunidade import models
+
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de dados criada")
+else:
+    print("Base de dados ja existente")
 
 from comunidade import routes
