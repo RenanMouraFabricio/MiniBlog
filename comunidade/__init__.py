8074a4bea8eec4bf768b29632373cd038f5f36e2
RenanMouraFabricio <165737048+RenanMouraFabricio@users.noreply.github.com>
2024-04-12 16:56:43 -0300

    Update __init__.py

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd75048462ea3f54faae38973937e33f8'
 
-if os.getenv("DATABASE_URL"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("postgres://comuindade_user:uT94EMkBzU5WXNXUWGHIQAsgrcu8ymxq@dpg-cocor7a0si5c73amtn9g-a.oregon-postgres.render.com/comuindade")
+if os.getenv("External Database URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("External Database URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
